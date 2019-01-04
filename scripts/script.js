1e102b8e5bb89691374926ca773b9e8206a79760
kolik29 <30481034+kolik29@users.noreply.github.com>
2019-01-04 12:48:51 +0300

    1

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,33 @@
+$(document).ready(function() {
+	$(window).on('scroll', function(){
+	    if ($(window).prop('pageYOffset') > 0) {
+	    	$('#main-slider > .shadow').css({
+	    		display: 'block'
+	    	});
+	    	setTimeout(function() {
+	    		$('#main-slider > .shadow').css({
+	    			opacity: 0.5
+	    		});
+	    	}, 1);
+	    	$('.hexaflip-cube.hexaflip-cube-slide').css({
+	    		transform: 'scale(0.95)'
+	    	})
+	    } else {
+	    	$('#main-slider > .shadow').css({
+	    		opacity: 0
+	    	});
+	    	setTimeout(function() {
+	    		$('#main-slider > .shadow').css({
+	    			display: 'none'
+	    		});
+	    	}, 500);
+	    	$('.hexaflip-cube.hexaflip-cube-slide').css({
+	    		transform: 'scale(1)'
+	    	})
+	    }
+	});
+});
+
 (function() {
 	document.addEventListener('DOMContentLoaded', function() {
 		var hexaSlider, leftControl, rightControl, mouseXStart, mouseXEnd;
@@ -10,10 +40,12 @@
 		rightControl = document.getElementById('slider-control--right');
 
 		leftControl.addEventListener('click', (function() {
-			return hexaSlider['flipBack']();
+    		$('html, body').animate({scrollTop: 0}, 500);
+    		setTimeout(hexaSlider['flipBack'](), 1000);
 		}), false);
 		rightControl.addEventListener('click', (function() {
-			return hexaSlider['flip']();
+    		$('html, body').animate({scrollTop: 0}, 500);
+    		setTimeout(hexaSlider['flip'](), 1000);
 		}), false);
 
 		document.getElementById('slider-container').addEventListener('mousedown', function(e) {
